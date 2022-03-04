0a835747f3e2ff76a4c9861af90603852d3f46b5
SamiraRa <53578188+SamiraRa@users.noreply.github.com>
2022-03-04 21:25:36 +0600

    user page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require('includes/header.php');
+require('includes/navbar.php'); 
+ ?>
+ 
+ <div class="py-5">
+     <div class="container">
+         <div class="row justify-content-center">
+             <div class="col-md-5">
+                 <div class="card">
+                     <div class="card-header">
+                         <h4>Login</h4>
+                     </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+
+<?php 
+require('includes/footer.php'); 
+?>
